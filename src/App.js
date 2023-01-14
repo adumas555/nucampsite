6a3644fad203721c1a870fdd730a86cd8458a4ca
Angela Dumas <adumas555@gmail.com>
2023-01-14 01:23:09 -0500

    CampsiteDetailPage with useParams

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ContactPage from "./pages/ContactPage";
+import CampsiteDetailPage from "./pages/CampsiteDetailPage";
 import "./App.css";
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="contact" element={<ContactPage />} />
         <Route path="directory" element={<CampsitesDirectoryPage />} />
+        <Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
       </Routes>
       <Footer />
     </div>
