c100bafd90b91c1d15cc1c556c9b1889ee22f0b5
Angela Dumas <adumas555@gmail.com>
2023-01-26 22:28:43 -0500

    Redux campsitesSlice

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -1,5 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
 import { CAMPSITES } from "../../app/shared/CAMPSITES";
-import CampsiteCard from "./CampsiteCard";
+
+const initialState = {
+  campsitesArray: CAMPSITES,
+};
+
+const campsiteSlice = createSlice({
+  name: "campsites",
+  initialState,
+});
+
+export const campsitesReducer = campsiteSlice.reducer;
 
 export const selectAllCampsites = () => {
   return CAMPSITES;
